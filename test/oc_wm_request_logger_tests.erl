e1b8aed432de15bc4a3872744556d8894fa24f5d
Ho-Sheng Hsiao <hosh@opscode.com>
2013-06-25 14:06:02 -0700

    Renamed tests to oc_wm_request_logger_tests

--- test/oc_wm_request_logger_tests.erl
+++ test/oc_wm_request_logger_tests.erl
@@ -1,8 +1,25 @@
--module(reporting_request_logger_tests).
+%% @copyright 2013 Opscode Inc.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% @author Matthew Peck <matthew@opscode.com>
+-module(oc_wm_request_logger_tests).
 
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("webmachine/include/webmachine_logger.hrl").
--include_lib("request_logger.hrl").
+-include_lib("sample_requests.hrl").
 
 valid_log_data() ->
   #wm_log_data{response_code = <<"200">>,
